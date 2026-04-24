7d95e2ac5a26b2aa469968aa8c79e6aae552f2fa
sergeiryzhkov <sergeyryzhkov2001@mail.ru>
2026-04-24 15:49:13 +0300

    added the ability to set date and time

--- app/src/main/java/com/work/todo/ui/addTask/AddTaskFragment.kt
+++ app/src/main/java/com/work/todo/ui/addTask/AddTaskFragment.kt
@@ -1,15 +1,19 @@
 package com.work.todo.ui.addTask
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.OvershootInterpolator
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.work.todo.R
 import com.work.todo.database.TaskCategory
 import com.work.todo.database.TaskDao
 import com.work.todo.database.TaskEntity
@@ -19,6 +23,9 @@ import com.work.todo.ui.home.category.CategoryItem
 import com.work.todo.ui.mapper.CategoryMapper
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AddTaskFragment : Fragment() {
 
@@ -30,6 +37,14 @@ class AddTaskFragment : Fragment() {
 
     private val taskDao: TaskDao by inject()
 
+    private var selectedDate: Calendar = Calendar.getInstance()
+    private var selectedTime: Calendar = Calendar.getInstance()
+
+    private val dateFormatter = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
+    private val timeFormatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+    private val dbDateFormatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,6 +55,9 @@ class AddTaskFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvSetDate.setOnClickListener { showDatePicker() }
+        binding.tvSetTime.setOnClickListener { showTimePicker() }
 
         setupUI()
 
@@ -56,9 +74,6 @@ class AddTaskFragment : Fragment() {
         val title = binding.etTaskName.text.toString().trim()
         val notes = binding.etNotes.text.toString().trim()
 
-        val date = "2024-05-20"
-        val time = "12:00"
-        val category = selectedCategory
 
         if (title.isEmpty()) {
             Toast.makeText(requireContext(), "Введите название задачи", Toast.LENGTH_SHORT).show()
@@ -67,9 +82,9 @@ class AddTaskFragment : Fragment() {
 
         val task = TaskEntity(
             title = title,
-            category = category,
-            date = date,
-            time = time,
+            category = selectedCategory,
+            date = dbDateFormatter.format(selectedDate.time),
+            time = timeFormatter.format(selectedTime.time),
             notes = notes,
             isDone = false
         )
@@ -152,5 +167,40 @@ class AddTaskFragment : Fragment() {
         isMenuExpanded = !isMenuExpanded
         binding.ivCategoryArrow.animate().rotation(if (isMenuExpanded) 180f else 0f).start()
         binding.llCategoryOptions.visibility = if (isMenuExpanded) View.VISIBLE else View.GONE
+    }
+
+    private fun showDatePicker() {
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year, month, dayOfMonth ->
+                selectedDate.set(year, month, dayOfMonth)
+
+                val tvDate = binding.tvSetDate.getChildAt(1) as TextView
+                tvDate.text = dateFormatter.format(selectedDate.time)
+                tvDate.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            },
+            selectedDate.get(Calendar.YEAR),
+            selectedDate.get(Calendar.MONTH),
+            selectedDate.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+    private fun showTimePicker() {
+        val timePickerDialog = TimePickerDialog(
+            requireContext(),
+            { _, hourOfDay, minute ->
+                selectedTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                selectedTime.set(Calendar.MINUTE, minute)
+
+                val tvTime = binding.tvSetTime.getChildAt(1) as TextView
+                tvTime.text = timeFormatter.format(selectedTime.time)
+                tvTime.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            },
+            selectedTime.get(Calendar.HOUR_OF_DAY),
+            selectedTime.get(Calendar.MINUTE),
+            true
+        )
+        timePickerDialog.show()
     }
 }
